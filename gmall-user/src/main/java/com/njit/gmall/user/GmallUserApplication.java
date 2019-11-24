971b44a696d5d2f04380327eb657b31020856b4b
njitzyd <909679349@qq.com>
2019-11-24 16:45:02 +0800

    修改测试

--- gmall-user/src/main/java/com/njit/gmall/user/GmallUserApplication.java
+++ gmall-user/src/main/java/com/njit/gmall/user/GmallUserApplication.java
@@ -1,9 +1,11 @@
 package com.njit.gmall.user;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class GmallUserApplication {
 
     public static void main(String[] args) {
